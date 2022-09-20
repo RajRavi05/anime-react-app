b2d8307d138a201980ab9d7a4053cf9102694d65
RajRavi05 <rajrv559910@gmail.com>
2022-09-20 11:04:04 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import HomePage from "./components/HomePage/HomePage";
 
 function App() {
   const [allData, setAllData] = useState([]);
+  const [apiError, setApiError] = useState(false);
 
-  const getAllSeriesData = async () => {
+  const getAllSeriesData = useCallback(async () => {
     let pageNo = 1;
     const API_DEFAULT = `https://api.jikan.moe/v4/anime`;
     const API_PAGINATION = `https://api.jikan.moe/v4/anime?page=${pageNo}`;
     const API_SEARCH = `https://api.jikan.moe/v4/anime?q=`;
 
-    const response = await fetch(API_PAGINATION);
-    const responseData = await response.json();
-    setAllData(responseData);
-  };
+    try {
+      const response = await fetch(API_PAGINATION);
+
+      if (!response.ok) {
+        throw new Error("Something went wrong. :(");
+      }
+
+      const data = await response.json();
+
+      const parsedSeries = data.data.map((item) => {
+        return {
+          title: item.title,
+          mal_id: item.mal_id,
+          imageURL: item.images.jpg.image_url,
+          rating: item.score,
+        };
+      });
+
+      setAllData(parsedSeries);
+    } catch (error) {
+      setApiError(true);
+    }
+  }, []);
 
   useEffect(() => {
     getAllSeriesData();
-  });
+  }, [getAllSeriesData]);
 
   return (
     <div>
       <Header />
-      <HomePage data={[]} />
+      {apiError && <HomePage list={allData} />}
     </div>
   );
 }
